5bec5321bcfc355db5b351d20745de05e324b0dc
Dominic Hayden <dominichayden7@gmail.com>
2025-03-17 12:49:37 -0500

    Updated config.py and ensured database connection works

--- app/config.py
+++ app/config.py
@@ -7,7 +7,8 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Stafdom1')
-    UPLOAD_FOLDER = os.environ.get('./uploads')
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed)
 
