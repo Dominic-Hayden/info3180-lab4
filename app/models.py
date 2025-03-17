0e6756ad1254e32a4ec3ce05dcec64dfdfc6bdd1
Dominic Hayden <dominichayden7@gmail.com>
2025-03-17 13:00:54 -0500

    Fixed password hashing method for 128-character limit

--- app/models.py
+++ app/models.py
@@ -22,9 +22,9 @@ class UserProfile(db.Model):
         self.username = username
         self.set_password(password)
 
-        
+
     def set_password(self, password):
-        self.password_hash= generate_password_hash(password)
+        self.password_hash= generate_password_hash(password, method= "pbkdf2:sha256")
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
